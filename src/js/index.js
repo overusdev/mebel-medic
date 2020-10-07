87e2bc45149f5c144c8ea3e30278a1b13dc0b3fa
Сергей Вилков <you@example.com>
2020-10-07 16:20:57 +0300

    Catalog begin

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,11 @@ import $ from "jquery";
 
 $(function () {
 
+    const $listItem = $('.main-catalog__tree-list-item');
+
+    $listItem.on( 'click', function(event) {
+        event.preventDefault();
+        $( this ).children('.main-catalog__tree-sublist').toggleClass('main-catalog__tree-sublist_state_active');
+    } );
+
 });
