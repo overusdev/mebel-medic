cc5963c964f6435165970f04110482792d3620ae
Сергей Вилков <you@example.com>
2020-10-09 11:53:57 +0300

    Without js logic

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,10 @@ import $ from "jquery";
 
 $(function () {
 
-    const $listItem = $('.main-catalog__tree-list-item');
+    const $listItem    = $('.main-catalog__tree-list-item');
+    const $sublistItem = $('.main-catalog__tree-sublist');
+
+    $sublistItem.hide();
 
     $listItem.on( 'click', function(event) {
         event.preventDefault();
